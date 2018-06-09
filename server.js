2979b4205d0536436f8765691b98cafd7a83e18b
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-06-08 20:05:34 -0500

    Initializes most basic files and makes it so basic content appears on page when on localhost:3000

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+// var logger = require("morgan");
+var mongoose = require("mongoose")
+
+// Scraping tools
+// var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Express
+var app = express();
+
+// Body-parser for form submission
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// Setting up handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Define the static directory
+app.use(express.static("public"));
+
+// Routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Connect to Mongo DB
+mongoose.connect("mongodb://localhost/mongo-scraper");
+
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+  });
